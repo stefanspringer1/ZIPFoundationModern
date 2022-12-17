8e728758bfee5d2ca49b2aa9e8d5828650290ffc
Greg Cotten <gregcotten@users.noreply.github.com>
2022-12-17 00:08:44 -0600

    Update FileManager+ZIP.swift

--- Sources/ZIPFoundation/FileManager+ZIP.swift
+++ Sources/ZIPFoundation/FileManager+ZIP.swift
@@ -235,6 +235,9 @@ public extension URL {
     func isContained(in parentDirectoryURL: URL) -> Bool {
         // Ensure this URL is contained in the passed in URL
         let parentDirectoryURL = URL(fileURLWithPath: parentDirectoryURL.path, isDirectory: true).standardized
+        #if os(Windows)
+        print("\"\(standardized.absoluteString)\" VS. \"\(parentDirectoryURL.absoluteString)\"")
+        #endif
         return standardized.absoluteString.hasPrefix(parentDirectoryURL.absoluteString)
     }
 }
